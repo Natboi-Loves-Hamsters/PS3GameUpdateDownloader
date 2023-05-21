cf9c9f9b376c582f6b422005bb4ca8d47f15f74f
natboi <nathanlegoblox@gmail.com>
2023-05-21 10:26:23 -0600

    Downloads files to new directory where PS3gud.py is. Added comments and deletes update.xml when done.

--- PS3gud.py
+++ PS3gud.py
@@ -1,33 +1,40 @@
 import requests
 import urllib3
-from urllib.parse import urlparse
 from xml.dom import minidom
+import os
 urllib3.disable_warnings()
 
-
-def download_url (url):
+def download_url(url):
     file_name_start_pos = url.rfind("/") + 1
     file_name = url[file_name_start_pos:]
-
+    # creates file name from the dl
     r = requests.get(url, stream=True)
     if r.status_code == requests.codes.ok:
-        with open(file_name, 'wb') as f:
+        with open(f"{name}/{file_name}", 'wb') as f:
             for data in r:
                 f.write(data)
     return url
+    # downloads the file into a directory
 
 name = input("What is the serial number of the game?: ")
 # name = "BCUS98208"
 version = input("What is the current version of the game?: ")
 # version = "01.28"
+# inputs for info
+
+path = os.path.exists(name)
+if path is False:
+    os.mkdir(name)
+# makes directory for the name if it doesnt exist
 
 URL = "https://a0.ww.np.dl.playstation.net/tpl/np/{}/{}-ver.xml".format(name, name)
 response = requests.get(URL, verify=False)
 if response.status_code != 200:
-    print ("Error, game ID is invalid!")
+    print("Error, game ID is invalid!")
     exit(1)
-with open('update.xml', 'wb') as file:
-   file.write(response.content)
+with open(f'{name}/update.xml', 'wb') as file:
+    file.write(response.content)
+# downloads the xml
 
 xmldoc = minidom.parse('update.xml')
 elements = xmldoc.getElementsByTagName('package')
@@ -39,7 +46,10 @@ print(f"Downloading {len(urls)} updates")
 for element in packagelist:
     updateurl = element.getAttribute("url")
     size = element.getAttribute("size")
-    sizekb = int(size)/1024
-    sizemb = sizekb/1024
+    sizemb = int(size)/1024/1024
     print(f"Size(MB): {sizemb:.2f} Downloading: {updateurl}")
     download_url(updateurl)
+# parses xml and downloads the pkg files
+
+os.remove(f'{name}/update.xml')
+# removes update.xml
